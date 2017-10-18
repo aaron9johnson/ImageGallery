31c208e4c8e2c1f2615820d1a999c4c2c4d336f8
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-17 19:04:42 -0700

    Initial Commit

---
+++ ImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGallery
+//
+//  Created by Aaron Johnson on 2017-10-17.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
